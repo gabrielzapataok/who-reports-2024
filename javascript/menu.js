b180000009fa3ef48f8c01d3e8a626eedff60e69
gabrielzapata <gaby.fz.58@gmail.com>
2024-04-19 10:56:51 -0300

    mega menu logic

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,51 +1,51 @@
 
 
 $(document).ready(function () {
+    
+    // Open close menu
     $('.button-menu').click(function () {
-        $('.menu .items').css('transform', 'translateX(0%)');
-    });
+        $('.menu .items').css('transform', 'translateX(0%)')
+    })
     $('.close-menu').click(function () {
-        $('.menu .items').css('transform', 'translateX(100%)');
-    });
+        $('.menu .items').css('transform', 'translateX(100%)')
+    })
+
+    // Resize window
     $(window).resize(function () {
         if ($(window).width() <= 800) {
-            $('.menu .items').css('transform', 'translateX(100%)');
+            $('.menu .items').css('transform', 'translateX(100%)')
         } else {
-            $('.menu .items').css('transform', 'translateX(0%)');
+            $('.menu .items').css('transform', 'translateX(0%)')
         }
-    });
-});
-
-
-
-
-
-$(document).ready(function () {
-    // Funcion menu
-    $('.button-menu').click(function () {
-        $('.menu .items').css('transform', 'translateX(0%)');
-    });
-    $('.close-menu').click(function () {
-        $('.menu .items').css('transform', 'translateX(100%)');
-    });
-    $(window).resize(function () {
-        if ($(window).width() <= 800) {
-            $('.menu .items').css('transform', 'translateX(100%)');
-        } else {
-            $('.menu .items').css('transform', 'translateX(0%)');
-        }
-    });
-    // Load outcomes
+        $('#menu-working-togheter .mega-menu-outcomes').removeClass('d-flex');
+        $('#menu-working-togheter .item_link').removeClass('active')
+        $('#menu-countries .mega-menu-outcomes').removeClass('d-flex');
+        $('#menu-countries .item_link').removeClass('active')
+    })
+    
+    // Load outcomes in working together
     $('.EOB-outcome-achievement').each(function (i, e) {
         var linkOutcome = $('<a>', {
             text: $(e).find('.outcome-title').text(),
             href: $(e).attr("href")
-        });
+        })
         $('.mega-menu-outcomes .container').append(linkOutcome)
     })
-    // Mega menu dropdown
+    
+    // Dropdown working together
     $('#menu-working-togheter').click(function () {
-        $('.mega-menu-outcomes').toggle();
+        $('#menu-countries .mega-menu-outcomes').removeClass('d-flex');
+        $('#menu-countries .item_link').removeClass('active')
+        $('#menu-working-togheter .mega-menu-outcomes').toggleClass('d-flex');
         $('#menu-working-togheter .item_link').toggleClass('active')
     });
-});
+
+    // Dropdown working together
+    $('#menu-countries .item_link').click(function () {
+        $('#menu-working-togheter .mega-menu-outcomes').removeClass('d-flex');
+        $('#menu-working-togheter .item_link').removeClass('active')
+        $('#menu-countries .mega-menu-outcomes').toggleClass('d-flex');
+        $('#menu-countries .item_link').toggleClass('active')
+    });
+
+})
